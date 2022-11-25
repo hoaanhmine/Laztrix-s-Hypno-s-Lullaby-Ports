cf0ecc1e56a89f1057662004ec60e8b316b2fdc2
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2022-11-25 16:58:12 +0700

    Add files via upload

--- assets/preload/data/missingno/script.lua
+++ assets/preload/data/missingno/script.lua
@@ -10,7 +10,6 @@ function onCreatePost()
     setSpriteShader('BG_Assets3','desaturation')
     setSpriteShader('ground','desaturation')
 
-  setSpriteShader('boyfriend','individualGlitches')
 
   setTextString('centerMark',' - ????????? [' .. string.upper(difficultyName) .. '] -')
 end
@@ -27,18 +26,6 @@ function onUpdate()
     else
         setProperty('defaultCamZoom', 0.8) -- normal is 0.85
     end
-    setShaderFloat('BG_Assets','frequency',-0)
-    setShaderFloat('BG_Assets2','frequency',-0)
-    setShaderFloat('BG_Assets3','frequency',-0)
-    setShaderFloat('ground','frequency',-0)
-
-        setShaderFloat('boyfriend','binaryIntensity',biner)
-        setShaderFloat('dad','binaryIntensity',biner)
-
-    setShaderFloat('BG_Assets','desaturationAmount',curdes)
-    setShaderFloat('BG_Assets2','desaturationAmount',curdes)
-    setShaderFloat('BG_Assets3','desaturationAmount',curdes)
-    setShaderFloat('ground','desaturationAmount',curdes)
 
 if curStep >= 320 and curStep <= 368 then 
     if curdes < 1 then
